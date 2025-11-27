1c16c0757e6ae2a4a524ad85a0a36099247bcbc3
Nurish <zhandarbekov.t.e@gmail.com>
2025-11-27 09:04:36 +0500

    Implement Customer class with cart and order methods

--- customer.py
+++ customer.py
@@ -1,1 +1,18 @@
+from shopping_cart import ShoppingCart
+from order import Order
+from payment import Payment
 
+class Customer:
+    def __init__(self, cid, name, email):
+        self.id = cid
+        self.name = name
+        self.email = email
+        self.cart = ShoppingCart()
+
+    def add_to_cart(self, product, qty):
+        self.cart.add_item(product, qty)
+
+    def place_order(self):
+        order = Order(oid=self.id * 1000, items=self.cart.items)
+        Payment(order.id, order.total).pay()
+        return order
