6b3b38b3d482fab76a59599678fe97a3147c9289
Nurish <zhandarbekov.t.e@gmail.com>
2025-11-27 09:02:17 +0500

    Add Product class with initialization and info method

--- product.py
+++ product.py
@@ -1,1 +1,8 @@
+class Product:
+    def __init__(self, pid, name, price):
+        self.id = pid
+        self.name = name
+        self.price = price
 
+    def get_info(self):
+        return f"{self.name} - {self.price}₸"
