2c7c1335e7a743d705d07fbbe35bad4272934a43
Nurish <zhandarbekov.t.e@gmail.com>
2025-11-27 09:03:44 +0500

    Add Order class with total calculation

--- order.py
+++ order.py
@@ -1,1 +1,8 @@
+class Order:
+    def __init__(self, oid, items):
+        self.id = oid
+        self.items = items
+        self.total = self.calculate_total()
 
+    def calculate_total(self):
+        return sum(i.get_subtotal() for i in self.items)
