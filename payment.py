99e2f65d8f169133487f82d94e06b348a9518229
Nurish <zhandarbekov.t.e@gmail.com>
2025-11-27 09:04:07 +0500

    Update payment.py

--- payment.py
+++ payment.py
@@ -1,1 +1,8 @@
+class Payment:
+    def __init__(self, order_id, amount):
+        self.order_id = order_id
+        self.amount = amount
 
+    def pay(self):
+        print(f"Payment of {self.amount}₸ for order {self.order_id} completed.")
+        return True
