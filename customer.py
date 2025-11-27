97742ea0008a28c56df338c3fbffb8d5ecc590a0
Nurish <zhandarbekov.t.e@gmail.com>
2025-11-27 09:17:58 +0500

    Update customer.py

--- customer.py
+++ customer.py
@@ -1,6 +1,6 @@
 from shopping_cart import ShoppingCart
 from order import Order
-from payment import Payment
+from payment_method import PaymentMethod
 
 class Customer:
     def __init__(self, cid, name, email):
@@ -12,7 +12,7 @@ class Customer:
     def add_to_cart(self, product, qty):
         self.cart.add_item(product, qty)
 
-    def place_order(self):
-        order = Order(oid=self.id * 1000, items=self.cart.items)
-        Payment(order.id, order.total).pay()
+    def place_order(self, payment: PaymentMethod):
+        order = Order(self.id * 1000, self.cart.items)
+        payment.pay(order.total)
         return order
