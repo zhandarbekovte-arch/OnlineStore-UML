7299e08a276baac6fa485dbcd709e91074f76de1
Nurish <zhandarbekov.t.e@gmail.com>
2025-11-27 09:04:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from product import Product
+from customer import Customer
 
+p1 = Product(1, "Laptop", 350000)
+p2 = Product(2, "Mouse", 5000)
+
+customer = Customer(1, "Yersin", "test@example.com")
+
+customer.add_to_cart(p1, 1)
+customer.add_to_cart(p2, 2)
+
+order = customer.place_order()
+print("Order total:", order.total)
