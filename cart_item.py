603e6049acf82f284d9ff58be897d2dc5ce5f2fd
Nurish <zhandarbekov.t.e@gmail.com>
2025-11-27 09:02:47 +0500

    Add CartItem class with subtotal calculation

--- cart_item.py
+++ cart_item.py
@@ -1,1 +1,7 @@
+class CartItem:
+    def __init__(self, product, quantity):
+        self.product = product
+        self.quantity = quantity
 
+    def get_subtotal(self):
+        return self.product.price * self.quantity
