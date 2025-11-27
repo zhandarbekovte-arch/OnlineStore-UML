b5bf019aa8e6e9e80a6d11d2a3637d64b85bc572
Nurish <zhandarbekov.t.e@gmail.com>
2025-11-27 09:03:20 +0500

    Update shopping_cart.py

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,1 +1,13 @@
+class ShoppingCart:
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, product, qty):
+        self.items.append(CartItem(product, qty))
+
+    def remove_item(self, product):
+        self.items = [i for i in self.items if i.product.id != product.id]
+
+    def get_total(self):
+        return sum(item.get_subtotal() for item in self.items)
 
