b2f51135168777fac3fda4f7068bfb4a5d0056be
Nurish <zhandarbekov.t.e@gmail.com>
2025-11-27 09:18:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from product import Product
 from customer import Customer
+from default_payment import DefaultPayment
 
 p1 = Product(1, "Laptop", 350000)
 p2 = Product(2, "Mouse", 5000)
@@ -9,5 +10,7 @@ customer = Customer(1, "Yersin", "test@example.com")
 customer.add_to_cart(p1, 1)
 customer.add_to_cart(p2, 2)
 
-order = customer.place_order()
+payment = DefaultPayment()   # Төлем түрі
+order = customer.place_order(payment)
+
 print("Order total:", order.total)
